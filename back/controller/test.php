0c4da6c7120f31f88e42c2c87deb4d88c25d0c9f
Breno-S <114250412+Breno-S@users.noreply.github.com>
2023-04-13 23:58:25 -0300

    Inserir passageiro

--- back/controller/test.php
+++ back/controller/test.php
@@ -1,4 +1,8 @@
 <!-- ARQUIVO SOMENTE PARA TESTES -->
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,26 +12,40 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+    if (isset($_SESSION['msg'])) {
+        echo $_SESSION['msg'];
+        unset($_SESSION['msg']);
+    }
+    ?>
     <form action="controller_passageiro.php" method="POST">
         <!-- input para o id do usurário associado ao passageiro -->
         <input type="hidden" name="id_usuario" value="1">
 
         <label for="">nome</label>
         <input type="text" name="nome" id="">
+
         <label for="">sobrenome</label>
         <input type="text" name="sobrenome" id="">
-        <label for="">cpf</label>
-        <input type="text" name="cpf" id="">
+
         <label for="">email</label>
-        <input type="email" name="" id="">
+        <input type="email" name="email" id="">
+
+        <label for="">cpf</label>
+        <input type="text" name="cpf" id="" maxlength="11">
+
         <label for="">data nascimento</label>
         <input type="date" name="data_nascimento" id="">
+
         <label for="">ddd</label>
         <input type="text" name="ddd" id="">
+
         <label for="">numero</label>
-        <input type="text" name="numero" id="">
+        <input type="text" name="numero" id="" maxlength="9">
+
         <label for="">tipo</label>
         <input type="text" name="tipo" id="">
+
         <input type="submit" value="Enviar">
     </form>
 </body>
