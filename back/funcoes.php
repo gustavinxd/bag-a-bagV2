8442c2bbb39c5d03ff24344d0d585c6822e73af9
Breno-S <114250412+Breno-S@users.noreply.github.com>
2023-04-13 22:45:52 -0300

    Update funcoes.php

--- back/funcoes.php
+++ back/funcoes.php
@@ -6,22 +6,22 @@ function validarRG($rg) {
     
     // Verificar se o número de RG tem 9 dígitos
     if (strlen($rg) != 9) {
-      return false;
+        return false;
     }
     
     // Calcular o dígito verificador
     $soma = 0;
     for ($i = 0; $i < 8; $i++) {
-      $soma += ($rg[$i] * (9 - $i));
+        $soma += ($rg[$i] * (9 - $i));
     }
     $dv = ($soma % 11);
     if ($dv == 10) {
-      $dv = 'X';
+        $dv = 'X';
     }
     
     // Verificar se o dígito verificador é igual ao último dígito do número de RG
     if ($dv != $rg[8]) {
-      return false;
+        return false;
     }
     
     // RG válido
@@ -36,37 +36,41 @@ function validarCPF($cpf) {
     
     // Verificar se o número de CPF tem 11 dígitos
     if (strlen($cpf) != 11) {
-      return false;
+        return false;
     }
     
     // Verificar se todos os dígitos são iguais
     if (preg_match('/^(\d)\1*$/', $cpf)) {
-      return false;
+        return false;
     }
     
     // Calcular o primeiro dígito verificador
     $soma = 0;
     for ($i = 0; $i < 9; $i++) {
-      $soma += ($cpf[$i] * (10 - $i));
+        $soma += ($cpf[$i] * (10 - $i));
     }
-    $dv1 = ($soma % 11);
-    if ($dv1 == 10) {
-      $dv1 = 0;
-    }
+    $resto = ($soma % 11);
+    if ($resto < 2) {
+        $dv1 = 0;
+    } else {
+		$dv1 = (11 - $resto);
+	}
     
     // Calcular o segundo dígito verificador
     $soma = 0;
-    for ($i = 0; $i < 10; $i++) {
-      $soma += ($cpf[$i] * (11 - $i));
+    for ($i = 1; $i < 10; $i++) {
+        $soma += ($cpf[$i] * (11 - $i));
     }
-    $dv2 = ($soma % 11);
-    if ($dv2 == 10) {
-      $dv2 = 0;
-    }
+    $resto = ($soma % 11);
+    if ($resto < 2) {
+        $dv2 = 0;
+    } else {
+		$dv2 = (11 - $resto);
+	}
     
     // Verificar se os dígitos verificadores são iguais aos últimos dois dígitos do CPF
     if ($dv1 != $cpf[9] || $dv2 != $cpf[10]) {
-      return false;
+        return false;
     }
     
     // CPF válido
