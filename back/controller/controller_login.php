707735698624157b3e0ce9bd73eca59d53cd64a0
1Guh <114250858+1Guh@users.noreply.github.com>
2023-04-20 15:15:30 -0300

    Atualização Back login usuário

--- back/controller/controller_login.php
+++ back/controller/controller_login.php
@@ -17,9 +17,9 @@ $senha_banco = $row_usuario['SENHA'];
 //Fazendo a validação da senha fornecida pelo usuário, e a cadastrada no db
 if ($senha_md5 == $senha_banco){
     
-    header("Location: ../apos_login.php?id_cadastro={$row_usuario['ID_CADASTRO']}");
+    header("Location:../../pages/user.php?id={$row_usuario['ID_CADASTRO']}");
 } else{
-    $_SESSION["msg"] = "<p style='color: red;'>Usuário inexistente / Login não realizado.</p>";
+    $_SESSION["msg"] = "<p style='color: red;'>Nome de usuário ou senha incorretos.</p>";
     header("Location: ../../pages/login.html");
 }
 ?>
