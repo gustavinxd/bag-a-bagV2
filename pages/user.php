c038fb146385d848c205649d39e3ccfabad8074b
gabrielmmateus <114250803+gabrielmmateus@users.noreply.github.com>
2023-04-29 16:29:53 -0300

    correção de caminho para home

--- pages/user.php
+++ pages/user.php
@@ -62,7 +62,7 @@
   <header id="header" class="fixed-top d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
 
-      <h1 class="logo"><a href="index.html">BAG-A-BAGₑ</a></h1>
+      <h1 class="logo"><a href="../index.html">BAG-A-BAGₑ</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
