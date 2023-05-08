a4b94e210c74ae92e815606f9399960e368e29e8
amrithomas <amri.righetti@gmail.com>
2023-05-08 13:52:30 -0300

    Revert "Merge remote-tracking branch 'origin/feat_back_passageiro' into feat_pages_admin"
    This reverts commit 9ee93ee1668e7abf54b9f7829050e393e77510dc, reversing
    changes made to f2a920ae3d55d6b762087172815fa827b390e886.

---
+++ back/controller/controller_editar_cadastro.php
@@ -0,0 +1,109 @@
+<?php
+    session_start();
+    include_once("../conexao.php");
+    include_once("../funcoes.php");
+
+    $id = filter_input(INPUT_POST, 'id' , FILTER_SANITIZE_NUMBER_INT);
+
+    $result_cadastro = "SELECT * FROM usuario 
+    INNER JOIN telefone ON FK_TELEFONE = ID_TELEFONE 
+    INNER JOIN cadastro ON FK_CADASTRO = ID_CADASTRO
+    INNER JOIN rg ON FK_RG = ID_RG
+    INNER JOIN endereco ON FK_ENDERECO = ID_ENDERECO
+    WHERE ID_USUARIO = '$id'";
+    $resultado_cadastro = mysqli_query($conn, $result_cadastro); //mysqli_query: executa uma consulta no banco de dados
+    $row_cadastro = mysqli_fetch_assoc($resultado_cadastro); //retornar a linha da tabela cadastro referente ao select
+
+    $nome = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
+    $sobrenome = filter_input(INPUT_POST, "ultname", FILTER_SANITIZE_STRING);
+    $nome_meio = filter_input(INPUT_POST, "middlename", FILTER_SANITIZE_STRING);
+
+    $cpf = filter_input(INPUT_POST, "cpf" );
+    $cpf = trim($cpf);
+    $cpf = str_replace(".","",$cpf);
+    $cpf = str_replace("-","",$cpf);
+
+    $rg = filter_input(INPUT_POST, "rg" );
+    $rg = trim($rg);
+    $rg = str_replace(".","",$rg);
+    $rg = str_replace("-","",$rg);
+
+    $data_emissao = filter_input(INPUT_POST, "data_emissao");
+    $data_nascimento = filter_input(INPUT_POST, "data_nasc");
+    $ddd = filter_input(INPUT_POST, "ddd", FILTER_SANITIZE_STRING);
+
+    $telefone = filter_input(INPUT_POST, "telefone", FILTER_SANITIZE_STRING);
+    $telefone = trim($telefone);
+    $telefone = str_replace("-","",$telefone);
+
+    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_STRING);
+
+    $cep = filter_input(INPUT_POST, "cep", FILTER_SANITIZE_STRING);
+    $cep = str_replace(".","",$cep);
+    $cep = str_replace("-","",$cep);
+
+    $cidade = filter_input(INPUT_POST, "cidade", FILTER_SANITIZE_STRING);
+    $uf = filter_input(INPUT_POST, "estado", FILTER_SANITIZE_STRING);
+    $rua = filter_input(INPUT_POST, "endereco", FILTER_SANITIZE_STRING);
+    $numero = filter_input(INPUT_POST, "numero", FILTER_SANITIZE_STRING);
+    $bairro = filter_input(INPUT_POST, "bairro", FILTER_SANITIZE_STRING);
+    $complemento = filter_input(INPUT_POST, "complemento", FILTER_SANITIZE_STRING);
+
+    // colocando o validador como falso
+    $data_rg_valida = false;
+
+    // fazendo as verificações do formulario
+    if (validarDataRg($data_emissao)){
+        $data_rg_valida = true;
+        echo "<br>";
+        echo "data de emissao do rg valido";
+    } //verificação da data de emissão do rg
+
+    if ($data_rg_valida){
+        $update_cadastro = "UPDATE cadastro SET EMAIL = '$email' WHERE id_cadastro = (SELECT FK_CADASTRO FROM usuario WHERE ID_USUARIO = '$id')";
+        $update_endereco = "UPDATE endereco SET CEP = '$cep', RUA = '$rua', NUMERO_ENDERECO = '$numero', BAIRRO = '$bairro', CIDADE = '$cidade', UF = '$uf', COMPLEMENTO = '$complemento' WHERE ID_ENDERECO= (SELECT FK_ENDERECO FROM usuario WHERE ID_USUARIO = '$id')";
+        $update_telefone = "UPDATE telefone SET DDD = '$ddd', NUMERO_TELEFONE = '$telefone' WHERE ID_TELEFONE= (SELECT FK_TELEFONE FROM usuario WHERE ID_USUARIO = '$id')";
+        $update_rg = "UPDATE rg SET DATA_EMISSAO = '$data_emissao' WHERE ID_RG= (SELECT FK_RG FROM usuario WHERE ID_USUARIO = '$id')";
+        $update_usuario = "UPDATE usuario SET NOME = '$nome', NOME_MEIO= '$nome_meio', SOBRENOME = '$sobrenome' WHERE ID_USUARIO= '$id'";
+        
+        $resultado_endereco = mysqli_query($conn, $update_endereco);
+        $resultado_cadastro = mysqli_query($conn, $update_cadastro);
+        $resultado_telefone = mysqli_query($conn, $update_telefone);
+        $resultado_rg = mysqli_query($conn, $update_rg);
+        $resultado_usuario = mysqli_query($conn, $update_usuario);
+
+        //verificar modificações
+        //verificar tabela de cadastro
+        if ($email != $row_cadastro['EMAIL']){
+            $update_cadastro_modificado = "UPDATE cadastro SET MODIFICADO = NOW() WHERE id_cadastro = (SELECT FK_CADASTRO FROM usuario WHERE ID_USUARIO = '$id')";
+            $resultado_cadastro_modificado = mysqli_query($conn, $update_cadastro_modificado);
+        }
+
+        //verificar tabela de endereço
+        if ($cep != $row_cadastro['CEP'] || $rua != $row_cadastro['RUA'] || $numero != $row_cadastro['NUMERO_ENDERECO'] || $bairro != $row_cadastro['BAIRRO'] || $cidade != $row_cadastro['CIDADE'] || $uf != $row_cadastro['UF'] || $complemento != $row_cadastro['COMPLEMENTO']){
+            $update_endereco_modificado = "UPDATE endereco SET MODIFICADO = NOW() WHERE ID_ENDERECO= (SELECT FK_ENDERECO FROM usuario WHERE ID_USUARIO = '$id')";
+            $resultado_endereco_modificado = mysqli_query($conn, $update_endereco_modificado);
+        }
+        
+        //verificar tabela de telefone
+        if ($telefone != $row_cadastro['NUMERO_TELEFONE'] || $ddd != $row_cadastro['DDD'] ){
+            $update_telefone_modificado = "UPDATE telefone SET MODIFICADO = NOW() WHERE ID_TELEFONE= (SELECT FK_TELEFONE FROM usuario WHERE ID_USUARIO = '$id')";
+            $resultado_telefone_modificado = mysqli_query($conn, $update_telefone_modificado);
+        }
+        
+        //verificar tabela de usuario
+        if ($nome != $row_cadastro['NOME'] || $nome_meio != $row_cadastro['NOME_MEIO'] || $sobrenome != $row_cadastro['SOBRENOME']){
+            $update_usuario_modificado = "UPDATE usuario SET MODIFICADO = NOW() WHERE ID_USUARIO= '$id'";
+            $resultado_usuario_modificado = mysqli_query($conn, $update_usuario_modificado);
+        }
+        
+        if (mysqli_affected_rows($conn)) {
+            //$_SESSION["msg"] = "<p style='color: blue;'>Usuário editado com sucesso</p>";
+            header("Location: ../../pages/user.php?id=$id");
+        } else {
+            //$_SESSION["msg"] = "<p style='color: blue;'>Usuário não foi editado.</p>";
+            header("Location: ../../pages/alteracoes_cadastro?id=$id");
+        }
+    }
+
+?>
---
+++ pages/pagamento.php
@@ -0,0 +1,333 @@
+<?php
+  session_start();
+  include_once('../back/conexao.php');
+
+  $id = $_SESSION['id_usuario'];
+
+  $query = "SELECT * FROM usuario 
+    INNER JOIN telefone ON FK_TELEFONE = ID_TELEFONE 
+    INNER JOIN cadastro ON FK_CADASTRO = ID_CADASTRO
+    INNER JOIN rg ON FK_RG = ID_RG
+    WHERE ID_USUARIO='$id'";
+    $query = mysqli_query($conn, $query);
+    $row = mysqli_fetch_assoc($query);
+    
+    if(empty($row)) {
+      header('Location: ../index.php');
+    }
+
+   
+    
+    
+  
+ //pegando o valor total do pagamento vindo da reserva
+ $valorTotal = $_SESSION['valor_total'];
+  
+  $parcelas = array();
+
+//Faz a divisão do valor total de 1 a 12 e armazena em um array para apresenta-lo posteriormente
+for ($i = 1; $i <= 12; $i++) {
+  $valorParcela = $valorTotal / $i; //Calcula o valor de cada parcela
+  $valorParcela = ceil($valorParcela * 100) / 100; // arredonda para cima com duas casas decimais
+  $valorParcelaFormatado = number_format($valorParcela, 2, ',', '.'); // formata o valor com duas casas decimais
+  array_push($parcelas, $valorParcelaFormatado); //Armazena a parcela no array de parcelas
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>>Bag-a-Bagₑ - Pagamento</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../assets/img/airplane_favicon.png" rel="icon">
+  <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com"> <!-- Fonte: Poppins -->
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <!-- Fonte: Poppins -->
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"> <!-- Fonte: Poppins -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../assets/css/style.css" rel="stylesheet">
+
+  <!-- CSS Bag-a-Bag -->
+  <link rel="stylesheet" href="../assets/css/pagamento/pagamento.css">
+
+  <!-- =======================================================
+  * Template Name: Groovin
+  * Updated: Mar 10 2023 with Bootstrap v5.2.3
+  * Template URL: https://bootstrapmade.com/groovin-free-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <h1 class="logo"><a href="../index.php">BAG-A-BAGₑ</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto" href="../index.php">HOME</a></li>
+          <li><a class="nav-link scrollto" href="../index.php#services">SOBRE</a></li>
+          <li><a class="nav-link scrollto" href="./destinos.php">DESTINOS</a></li>
+          <li><a class="nav-link scrollto " href="../index.php#pricing">OFERTAS</a></li>
+          <li><a class="nav-link scrollto" href="../index.php#contact">CONTATO</a></li>
+          <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul> -->
+          </li>
+          <li><a class="getstarted scrollto" href="<?php echo "user.php?id=" . $row['ID_USUARIO'] ?>" style = "margin-left: 80px;">Ver perfil</a></li>
+          <li><a class="nav-link scrollto" href="../back/controller/controller_logoff.php" >LOGOFF</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+
+  <!-- conteudo de pagamento -->
+    <main class="container">
+        <form action="" method="post">
+            <div class="row">
+                <h3 class="h-faturamento">Detalhes do faturamento</h3>
+                <div id="caixa-detalhes" class="col-8 offset-2 shadow">
+                <div class="row">
+                    <div class="half-box">
+                        <div class="col">
+                          <label for = "name" class="required" style = "color: #5c9f24">Nome</label>
+                          <input type = "name" value="<?php echo ucfirst($row['NOME']) ?>" class="form-control required" style = "background-color: #FFF; border-color: black" name = "name" id = "name" disabled>
+                      </div>
+                    </div>
+                    <div class = "half-box">
+                          <label for = "lastname" class="required" style = "color: #5c9f24">Sobrenome</label>
+                          <input type = "lastname" value= "<?php echo ucfirst($row['SOBRENOME']) ?>" class="form-control required" style = "background-color: #FFF; border-color: black" name = "sobrenome" id = "sobrenome" disabled>
+                        <br>
+                    </div>
+                </div>
+                <div class="row">
+                      <div class = "half-box">
+                        <label for = "cpf" class="required" style = "color: #5c9f24" >CPF</label>
+                        <input type = "text" value="<?php echo $row['CPF'] ?>" class="form-control required" style = "background-color: #FFF; border-color: black" name = "cpf" id = "cpf" disabled>
+                    </div>
+                    <div class = "half-box">
+                        <label for = "rg" class="required" style = "color: #5c9f24">RG</label>
+                        <input type = "text" value="<?php echo $row['NUMERO_RG'] ?>" name = "rg" id = "rg" class="form-control required" style = "background-color: #FFF; border-color: black" disabled>
+                      <br>
+                    </div>
+                </div>
+                  <div class="row">
+                      <div class = "half-box">
+                          <label for = "email" class="required" style = "color: #5c9f24">E-mail</label>
+                          <input type = "email" value="<?php echo $row['EMAIL'] ?>" name = "email" id = "email" class="form-control required" style = "background-color: #FFF; border-color: #000" disabled>
+                        </div>
+                        <div class = "half-box">
+                          <label for = "tel" class="required" style = "color: #5c9f24">Telefone</label>
+                          <input type = "tel" value = "<?php echo $row['DDD'] . ' ' . $row['NUMERO_TELEFONE']?>" name = "telefone" id = "telefone" placeholder = "(DDD)XXXXX-XXXX" maxlength="15" class="form-control required" onkeypress="tel(this)" style = "background-color: #FFF; border-color: black" disabled>
+                        <br>
+                    </div>
+
+                </div>
+            </div>
+         </div>
+     </form>
+
+        
+        <form action="../back/controller/controller_verificar_pagamento.php" method="post">
+          <div class="row">
+            <h3 class="h-forma">Formas de pagamento</h3>
+            <div id="caixa-pagamento" class="col-8 offset-2 shadow">
+
+                <input type="radio" id="termos" name="pagamento"  value="credito"> <label for="">Cartão de crédito</label> 
+                <div id="termoConteudo">
+
+                  <div class="row">
+                    <div class="half-box">
+                      <div class="col">
+                        <label for = "name" class="required" style = "color: #5c9f24">Nome impresso no cartão</label>
+                        <input type = "name" class="form-control required" style = "background-color: #FFF; border-color: black" name = "name" id = "name">
+                        
+                      </div>
+                    </div>
+
+                    <div class = "half-box">
+                        <label for = "lastname" class="required" style = "color: #5c9f24">Número do cartão</label>
+                        <input type = "lastname" name="numeroCartao" class="form-control required" style = "background-color: #FFF; border-color: black" name = "sobrenome" id = "sobrenome" maxlength="19">
+                        <br>
+                      </div>
+                    </div>
+                  
+                  <div class="row">
+                    <div class = "half-box">
+                      <label for = "cpf" class="required" style = "color: #5c9f24" >Bandeira do cartão</label>
+                      <input type = "text" class="form-control required" style = "background-color: #FFF; border-color: black" name = "cpf" id = "cpf">
+                    </div>
+
+                    <div class = "half-box">
+                      <label for = "rg" class="required" style = "color: #5c9f24">Validade</label>
+                      <input type = "text" name = "dataValidade" id = "rg" placeholder="MM/AAAA" class="form-control required" style = "background-color: #FFF; border-color: black" maxlength="7">
+                      <br>
+                    </div>
+                  </div>
+
+                  <div class="row">
+                      <div class = "half-box">
+                        <label for = "email" class="required" style = "color: #5c9f24">Código de segurança</label>
+                        <input type = "text" name = "email" id = "email" placeholder="CVV" class="form-control required" style = "background-color: #FFF; border-color: #000" maxlength="4">
+                      </div>
+
+                      <div class = "half-box">
+                      <label for = "parcelas" class="required" style = "color: #5c9f24">Parcelas</label>
+                      <select name="parcelas" id="parcelas" class="form-control required" style = "background-color: #FFF; border-color: black">
+                        <?php foreach ($parcelas as $key => $valorParcela): ?>  <!-- estrutura de repetição -->
+                        <option value="<?php echo $key+1; ?>"><?php echo ($key+1).'x de R$ '.$valorParcela; ?></option>  <!-- Número de parcelas, e seu respectivo valor -->
+                        <?php endforeach; ?> <!-- Fim da estrutura de repetição -->
+                      </select>
+                      
+                      <br>
+                    </div>
+                  </div>
+                    
+                </div>
+
+                    <br>
+                    <hr class="linha">
+                    <input type="radio" id="termos2" name="pagamento" value="pix"> <label for=""> Pagar com pix </label> 
+                    <div id="termoConteudo2">           
+                      <img src="/assets/img/pagamento/qrcode.png" id="pix" alt="" srcset="">    
+                      <!-- valor total da passagem -->
+
+                    </div>
+                    <hr class="linha">
+                    <input type="radio" id="termos3" name="pagamento" value="boleto"> <label for=""> Boleto </label> 
+                    <i class="bi bi-upc-scan"></i>
+                    <div  id="termoConteudo3">
+                      <img src="/assets/img/pagamento/codigo.jpg" id="boleto" alt="" srcset="">
+                      <!-- valor total da passagem -->
+
+                    </div>
+                    <br>
+                    <br>
+                    <button type="submit" id="btn-submit" style="min-width: 80px;" class="btn btn-primary">Enviar</button>
+                    <br>
+            </div>
+          </div>
+            <br>
+        </form>
+
+        
+    </main>  
+    <!-- fim conteudo pagamento -->
+
+    <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6">
+            <div class="footer-info">
+              <h3>BAG-A-BAGₑ</h3>
+              <p>
+                Av. Washington Luís<br>
+                Vila Congonhas<br>
+                São Paulo - SP N°4878<br><br>
+                <strong>Telefone:</strong>(11) 9000-0000<br>
+                <strong>Email:</strong>bag.a.bag@gmail.com<br>
+              </p>
+              <div class="social-links mt-3">
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>BAG-A-BAGₑ</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="../index.php">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="../index.php#services">Sobre</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="./destinos.php">Destinos</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="../index.php#pricing">Ofertas</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="../index.php#contact">Contato</a></li>
+            </ul>
+          </div>
+
+        <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Conta</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="<?php echo "user.php?id=" . $row['ID_USUARIO'] ?>">Ver perfil</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="../back/controller/controller_logoff.php">Logoff</a></li>
+              <!-- <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li> -->
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Inscreva-se para receber ofertas exclusivas</h4>
+            <!-- <p>Inscreva-se para receber ofertas exclusivas</p> -->
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Quero recebê-las!">
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Groovin</span></strong>. All rights Reserved.
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/groovin-free-bootstrap-theme/ -->
+        Designed by  <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+    <!-- Vendor JS Files -->
+  <script src="../assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="../assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../assets/js/main.js"></script>
+</body>
