42b0902173d479a2bf2e52f123391bffa9ecd4c1
amrithomas <amri.righetti@gmail.com>
2023-05-08 14:45:11 -0300

    update

--- back/controller/controller_logoff.php
+++ back/controller/controller_logoff.php
@@ -1,5 +1,5 @@
 <?php 
     session_start();
     unset($_SESSION['id_usuario']);
-    echo "<script>location.href='./login.php';</script>";
+    echo "<script>location.href='../../index.php';</script>";
 ?>
--- pages/user.php
+++ pages/user.php
@@ -77,40 +77,39 @@ if (!empty($row3['FK_AVIAO_IDA']) && !empty($row2['FK_AVIAO_VOLTA'])) {
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta charset="utf-8" />
+  <meta content="width=device-width, initial-scale=1.0" name="viewport" />
 
-  <title>BAG-A-BAGₑ</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+  <title>Bag-a-Bagₑ - User</title>
+  <meta content="" name="description" />
+  <meta content="" name="keywords" />
 
   <!-- Favicons -->
-  <link href="../assets/img/favicon.png" rel="icon">
-  <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="../assets/img/airplane_favicon.png" rel="icon" />
+  <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet" />
+  <!-- Import CSS Files -->
+  <link rel="stylesheet" href="../assets/css/destinos/destinos.css">
   <!-- Vendor CSS Files -->
-  <link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-  <!-- CSS files  -->
-  <link rel="stylesheet" href="../assets/css/user/user.css">
+  <link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet" />
+  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
+  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
+  <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
+  <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+
   <!-- Template Main CSS File -->
-  <link href="../assets/css/style.css" rel="stylesheet">
+  <link href="../assets/css/style.css" rel="stylesheet" />
 
   <!-- =======================================================
-  * Template Name: Groovin
-  * Updated: Mar 10 2023 with Bootstrap v5.2.3
-  * Template URL: https://bootstrapmade.com/groovin-free-bootstrap-theme/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
+        * Template Name: Groovin
+        * Updated: Mar 10 2023 with Bootstrap v5.2.3
+        * Template URL: https://bootstrapmade.com/groovin-free-bootstrap-theme/
+        * Author: BootstrapMade.com
+        * License: https://bootstrapmade.com/license/
+        ======================================================== -->
 </head>
 
 <body>
@@ -118,34 +117,52 @@ if (!empty($row3['FK_AVIAO_IDA']) && !empty($row2['FK_AVIAO_VOLTA'])) {
   <header id="header" class="fixed-top d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
 
-      <h1 class="logo"><a href="index.html">BAG-A-BAGₑ</a></h1>
+      <h1 class="logo"><a href="../index.php">BAG-A-BAGₑ</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">HOME</a></li>
-          <li><a class="nav-link scrollto" href="#">SOBRE</a></li>
-          <li><a class="nav-link scrollto" href="#">DESTINOS</a></li>
-          <li><a class="nav-link scrollto " href="#">OFERTAS</a></li>
-          <li><a class="nav-link scrollto" href="#">CONTATO</a></li>
+          <li><a class="nav-link scrollto " href="../index.php">HOME</a></li>
+          <li><a class="nav-link scrollto" href="../index.php#about">SOBRE</a></li>
+          <li><a class="nav-link scrollto active" href="./destinos.php">DESTINOS</a></li>
+          <li><a class="nav-link scrollto " href="../index.php#pricing">OFERTAS</a></li>
+          <li><a class="nav-link scrollto" href="../index.php#contact">CONTATO</a></li>
           <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul> -->
+              <ul>
+                <li><a href="#">Drop Down 1</a></li>
+                <li><a href="#">Drop Down 2</a></li>
+                <li><a href="#">Drop Down 3</a></li>
+                <li><a href="#">Drop Down 4</a></li>
+              </ul> -->
           </li>
-          <li><a class="nav-link scrollto" href="#contact" style="margin-left: 80px;">LOGIN</a></li>
-          <li><a class="getstarted scrollto" href="#about">CADASTRE-SE</a></li>
+          <?php
+          //VERIFICANDO SE TEM UM USUARIO LOGADO
+          if (isset($_SESSION['id_usuario'])) {
+            $id = $_SESSION['id_usuario'];
+
+            $query = "SELECT * FROM usuario 
+              INNER JOIN telefone ON FK_TELEFONE = ID_TELEFONE 
+              INNER JOIN cadastro ON FK_CADASTRO = ID_CADASTRO
+              WHERE ID_USUARIO='$id'";
+            $query = mysqli_query($conn, $query);
+            $row = mysqli_fetch_assoc($query);
+            //SE ESTIVER LOGADO APARECERÁ AS SEGUINTES INFORMAÇÕES
+            echo '<li><a class="getstarted scrollto" href="user.php?id=' . $row["ID_USUARIO"] . '" style="margin-left: 80px;">Ver perfil</a></li>';
+            echo '<li><a class="nav-link scrollto" href="../back/controller/controller_logoff.php">LOGOFF</a></li>';
+          } else {
+            //SE NÃO ESTIVER LOGADO APARECERÁ AS SEGUINTES INFORMAÇÕES
+            echo '<li><a class="nav-link scrollto" href="login.html" style="margin-left: 80px;">LOGIN</a></li>';
+            echo '<li><a class="getstarted scrollto" href="cadastro.php">CADASTRE-SE</a></li>';
+          }
+          ?>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
 
     </div>
-  </header><!-- End Header -->
-
+  </header>
+  <!-- End Header -->
   <main id="main" class="mt-5 container">
     <section class="row d-flex align-items-center justify-content-center" id="profile">
       <img src="../assets/img/user/icone_perfil.png" alt="" class="col-lg-2 col-sm-6">
