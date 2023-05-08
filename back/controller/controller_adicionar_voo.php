6883f5171ef1442a4fc0d22302ecb2b4cfde4db2
Breno-S <114250412+Breno-S@users.noreply.github.com>
2023-05-08 01:33:44 -0300

    Update controller_adicionar_voo.php
    é possível criar voos

--- back/controller/controller_adicionar_voo.php
+++ back/controller/controller_adicionar_voo.php
@@ -25,14 +25,14 @@ $horario_escala_volta = filter_input(INPUT_POST, 'horario_volta_escala');
 $tempo_escala_volta = filter_input(INPUT_POST, 'tempo_escala_volta');
 
 
-echo $aviao_ida . "<br>";
-echo $aviao_volta . "<br>";
+// echo $aviao_ida . "<br>";
+// echo $aviao_volta . "<br>";
 
-echo $aeroporto_origem . "<br>";
-echo $aeroporto_destino . "<br>";
+// echo $aeroporto_origem . "<br>";
+// echo $aeroporto_destino . "<br>";
 
-echo $aeroporto_escala_ida . "<br>";
-echo $aeroporto_escala_volta . "<br>";
+// echo $aeroporto_escala_ida . "<br>";
+// echo $aeroporto_escala_volta . "<br>";
 
 // FORMATAR O VALOR DE MOEDA PARA O PADRÃO FLOAT
 $valor_passagem = filter_input(INPUT_POST, 'valor_passagem');
@@ -44,34 +44,41 @@ $valor_passagem = (float) $valor_passagem;
 
 
 // Verifica se escolheu escala
-if (!empty($aeroporto_escala_ida) && !empty($aeroporto_escala_volta)) {
-
+if (!empty($aeroporto_escala_ida)) {
 	$query = "INSERT INTO escala (HORARIO_CHEGADA_ESCALA, TEMPO_ESCALA, FK_AEROPORTO_ESCALA) VALUES ('$horario_escala_ida', '$tempo_escala_ida', '$aeroporto_escala_ida')";
 	mysqli_query($conn, $query);
+	$fk_escala_ida = mysqli_insert_id($conn);
+} else {
+	$fk_escala_ida = 'NULL';
+}
+
+if (!empty($aeroporto_escala_volta)) {
 	$query = "INSERT INTO escala (HORARIO_CHEGADA_ESCALA, TEMPO_ESCALA, FK_AEROPORTO_ESCALA) VALUES ('$horario_escala_volta', '$tempo_escala_volta', '$aeroporto_escala_volta')";
 	mysqli_query($conn, $query);
-}
-// INSERIR DADOS NA TABELA DE ESCALA
-if (mysqli_insert_id($conn)) {
-	$aeroporto_escala_ida = mysqli_insert_id($conn);
-	$aeroporto_escala_volta = mysqli_insert_id($conn);
+	$fk_escala_volta = mysqli_insert_id($conn);
 } else {
-	$aeroporto_escala_ida = "NULL";
-	$aeroporto_escala_volta = "NULL";
-	// INSERIR DADOS NA TABELA DE VOOS
-	$query = "INSERT INTO voo 
-		(FK_ORIGEM_AERO, FK_DESTINO_AERO, FK_ESCALA_IDA, FK_ESCALA_VOLTA, VALOR_PASSAGEM, IDA_HORARIO_PARTIDA, IDA_HORARIO_CHEGADA, VOLTA_HORARIO_PARTIDA, VOLTA_HORARIO_CHEGADA, FK_AVIAO_IDA, FK_AVIAO_VOLTA, CRIADO) 
-		VALUES ('$aeroporto_origem', '$aeroporto_destino', '$aeroporto_escala_ida', '$aeroporto_escala_volta', '$valor_passagem', '$horario_partida_ida', '$horario_chegada_ida', '$horario_partida_volta', '$horario_chegada_volta', '$aviao_ida', '$aviao_volta', NOW())";
-	
-	mysqli_query($conn, $query);
-	if (mysqli_insert_id($conn)) {
-		$_SESSION['msg'] = "<p style='color: green;' class='text-center'>VOO CADASTRADO COM SUCESSO!</p>";
-		echo "<script>location.href='../admin/voo.php'</script>";
-	} else {
-		$_SESSION['msg'] = "<p style='color: red;' class='text-center'>ERRO!</p>";
-		echo "<script>location.href='../admin/criar_voo.php'</script>";
-	}
+	$fk_escala_volta = "NULL";
 }
 
+// INSERIR VOO NA TABELA DE VOO
+if (empty($aviao_volta)) {
+	$query = "INSERT INTO voo 
+	(FK_ORIGEM_AERO, FK_DESTINO_AERO, FK_ESCALA_IDA, VALOR_PASSAGEM, IDA_HORARIO_PARTIDA, IDA_HORARIO_CHEGADA, FK_AVIAO_IDA, CRIADO) 
+	VALUES ('$aeroporto_origem', '$aeroporto_destino', $fk_escala_ida, '$valor_passagem', '$horario_partida_ida', '$horario_chegada_ida', '$aviao_ida', NOW())";
+} else {
+	$query = "INSERT INTO voo 
+	(FK_ORIGEM_AERO, FK_DESTINO_AERO, FK_ESCALA_IDA, FK_ESCALA_VOLTA, VALOR_PASSAGEM, IDA_HORARIO_PARTIDA, IDA_HORARIO_CHEGADA, VOLTA_HORARIO_PARTIDA, VOLTA_HORARIO_CHEGADA, FK_AVIAO_IDA, FK_AVIAO_VOLTA, CRIADO) 
+	VALUES ('$aeroporto_origem', '$aeroporto_destino', '$fk_escala_ida', '$fk_escala_volta', '$valor_passagem', '$horario_partida_ida', '$horario_chegada_ida', '$horario_partida_volta', '$horario_chegada_volta', '$aviao_ida', '$aviao_volta', NOW())";
+}
 
-	
+mysqli_query($conn, $query);
+
+if (mysqli_insert_id($conn)) {
+	$_SESSION['msg'] = "<p style='color: green;' class='text-center'>VOO CADASTRADO COM SUCESSO!</p>";
+	echo "<script>location.href='../admin/voo.php'</script>";
+} else {
+	$_SESSION['msg'] = "<p style='color: red;' class='text-center'>ERRO!</p>";
+	echo "<script>location.href='../admin/criar_voo.php'</script>";
+}
+
+?>
